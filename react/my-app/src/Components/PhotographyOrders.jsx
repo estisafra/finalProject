5fb6ebfbf7cde2540304293930fc6d504db5cc21
esti <s0504172669@gmail.com>
2025-04-04 03:12:14 +0300

    component photographyOrder

--- react/my-app/src/Components/PhotographyOrders.jsx
+++ react/my-app/src/Components/PhotographyOrders.jsx
@@ -1,18 +1,17 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { useSelector } from "react-redux";
-import { useSelector } from 'react-redux';import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 const  PhotographyOrders = () => {
     const[orders, setOrders] = useState([])
-    const id = useSelector((state) => state.user.id); 
+     const id = useSelector((state) => state.user.id); 
+     
     useEffect(() => {
-
+       
         if (id) { 
-            axios.get("https://localhost:8080/Photography/getOrdersByPhotography/", {
-                params: { phtographyId: id } 
-            })
+            axios.get(`http://localhost:8080/Order/getOrdersByPhotography/${id}`)
             .then(response => {
-                setOrders(response.data); 
+                console.log(response.data);
+                setOrders(response.data);
             })
             .catch(error => {
                 console.error("Error fetching orders:", error);
@@ -20,13 +19,18 @@ const  PhotographyOrders = () => {
         }
     }, [id]); 
     return(
-      
+        
         <div>
             <h1>Photography Orders</h1>
             <ul>
-                {orders.map((order, index) => (
-                    <li key={index}>{order.name}</li>
-                ))}
+            <ul>
+           {orders.map((order, index) => (
+           <li key={index}>
+            Order User: {order.orderUser} <br />
+            Order Date: {new Date(order.orderDate).toLocaleDateString()}
+           </li>
+    ))}
+</ul>
             </ul>
         </div>
     )
